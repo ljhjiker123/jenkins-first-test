68708a4ae6d8bc8b8569bbb848bf84c0ef5d798c
ljhjiker123 <810758134@qq.com>
2020-03-18 16:22:02 +0800

    设置tomcat

--- src/main/java/com/jiker/jenkins/first/test/JenkinsFirstTestApplication.java
+++ src/main/java/com/jiker/jenkins/first/test/JenkinsFirstTestApplication.java
@@ -2,9 +2,16 @@ package com.jiker.jenkins.first.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsFirstTestApplication {
+public class JenkinsFirstTestApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsFirstTestApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsFirstTestApplication.class, args);
