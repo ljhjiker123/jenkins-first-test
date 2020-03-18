b182b6cf5c51bc246214ee1ad4d8cb55994b41eb
ljhjiker123 <810758134@qq.com>
2020-03-18 10:06:12 +0800

    创建项目

---
+++ src/main/java/com/jiker/jenkins/first/test/JenkinsFirstTestApplication.java
@@ -0,0 +1,13 @@
+package com.jiker.jenkins.first.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFirstTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsFirstTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiker/jenkins/first/test/JenkinsFirstTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiker.jenkins.first.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsFirstTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
